74bbfbe5b973e5948788748aa7c93f9229991e63
Kelty Tech <Kelvinlinnaeus@gmail.com>
2024-04-25 05:43:45 +0000

    Added Opening Functionality

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pyttsx3
 import speech_recognition as sr
+import keyboard
+import os
+import subprocess as sp
 
 from decouple import config
 from datetime import datetime
@@ -23,27 +26,43 @@ def speak(text):
 
 def greet_me():
   hour = datetime.now().hour
-  if (hour >= 6) and (hour < 12):
+  if (hour >= 4) and (hour < 12):
     speak(f"Good morning {user}")
   elif (hour >= 12) and (hour <= 16):
     speak(f"Good afternoon {user}")
   elif (hour >= 16) and (hour < 19):
     speak(f"Good evening {user}")
-  speak(f"It's {hostname}. How can I be of assist {user}")
+  speak(f"It's {hostname}. How can I be of assist sir")
+  
+listening = False
+def start_listening():
+  global listening
+  listening = True
+  print("Listening")
+
+def pause_listening():
+  global listening
+  listening = False
+  print("Listening Stopped")
+
+# keyboard.add_hotkey("ctrl+alt+p", pause_listening)
+# keyboard.add_hotkey("ctrl+alt+l", start_listening)
 
 
 def take_command():
   r = sr.Recognizer()
-  with sr.Microphone as source:
+  with sr.Microphone() as source:
     print("Listening ...")
     r.pause_threshold = 1
     audio = r.listen(source)
 
   try:
     print("Recognizing ...")
-    query = r.recognize_google_cloud(audio, language="en-in")
-    print(query)
-    if not "stop" in query or "exit" in query:
+
+    queri = r.recognize_google(audio, language="en-us")
+    print(queri)
+    print(queri)
+    if not "stop" in queri or "exit" in queri or "cancel" in queri or "it's okay" in queri:
       speak(choice(random_text))
     else:
       hour = datetime.now().hour
@@ -55,13 +74,27 @@ def take_command():
 
   except Exception:
     speak("Sorry didn't grab that boss. Could you come again?")
-    query = "None"
-  return query
+    queri = "None"
+  return queri
 
 
 if __name__ == '__main__':
-  # greet_me()
+  greet_me()
   while True:
-    query = take_command().lower()
-    if "how are you" in query:
-      speak("I good boss. What about you?")
+    if not listening:
+      query = take_command().lower()
+      if "how are you" in query:
+        speak("I good boss. What about you?")
+
+      elif "open command prompt" in query:
+        speak("Opening command prompt")
+        os.system("start cmd")
+
+      elif "open camera" in query:
+        speak("Opening camera sir")
+        sp.run("start microsoft.windows.camera:", shell=True)
+
+      elif "open notepad" in query:
+        speak("Opening Notepad for you")
+        notepad_path = "C:\\Users\\kelvi\\AppData\\Local\\Microsoft\\WindowsApps\\notepad.exe"
+        os.startfile(notepad_path)
